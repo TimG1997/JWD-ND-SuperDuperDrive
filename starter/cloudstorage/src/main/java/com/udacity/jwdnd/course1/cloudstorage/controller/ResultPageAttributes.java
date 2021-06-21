1370d6e74cf9567e8c3459df6ad2ae52180a8ebf
Tim Grein <tim@4greins.de>
2021-06-21 16:02:45 +0200

    Add ResultController and ResultPageAttributes.

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/ResultPageAttributes.java
@@ -0,0 +1,9 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+public interface ResultPageAttributes {
+
+    String SUCCESS_FLAG = "success";
+    String ERROR_FLAG = "error";
+    String ERRORS_LIST = "errors";
+
+}
